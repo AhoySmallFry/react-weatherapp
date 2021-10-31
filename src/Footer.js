dc618997739cc9e096ea32b4b4da5db93d425948
AhoySmallFry <jordana@makin.co>
2021-10-31 15:28:16 +0000

    added rel no referrer to link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="Footer">
-      <a href="https://github.com/AhoySmallFry" target="_blank">
+      <a
+        href="https://github.com/AhoySmallFry"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code
       </a>
       &nbsp;by Jordana Makin
