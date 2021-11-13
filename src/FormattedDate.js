65c92c331293e42f6e390ed31efb13e8379e9015
AhoySmallFry <jordana@makin.co>
2021-11-13 19:33:18 +0000

    added starting 0 to hours and mintues

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -12,7 +12,14 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   let hour = props.date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
   let minutes = props.date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
   return (
     <div>
