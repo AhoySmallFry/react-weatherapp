6424ebafe6140fad9471da32df40327df6ab67ec
AhoySmallFry <jordana@makin.co>
2021-11-18 17:27:16 +0000

    Forecast for one day functions

--- src/DayForecast.js
+++ src/DayForecast.js
@@ -2,13 +2,21 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function DayForecast(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div>
-      <div className="forecast-day">{props.data.dt}</div>
+      <div className="forecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} />
       <div className="weather-forecast-temperature">
-        <strong>{Math.round(props.data.temp.max)}</strong>
-        {Math.round(props.data.temp.min)}
+        <strong>{Math.round(props.data.temp.max)}°|</strong>
+        {Math.round(props.data.temp.min)}°
       </div>
     </div>
   );
